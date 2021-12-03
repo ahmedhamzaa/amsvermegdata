0247113a1635406c67c1f76bdcf7c2e01013c0da
ahmed hamza jandoubi <ahmedhamza0601@gmail.com>
2021-12-03 11:20:13 +0100

    first commit

---
+++ ams_vermeg-data/src/test/java/com/sip/ams_vermegdata/AmsVermegDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sip.ams_vermegdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsVermegDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ amsvermegdata/src/main/java/com/sip/amsvermegdata/AmsvermegdataApplication.java
@@ -0,0 +1,13 @@
+package com.sip.amsvermegdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsvermegdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmsvermegdataApplication.class, args);
+    }
+
+}
