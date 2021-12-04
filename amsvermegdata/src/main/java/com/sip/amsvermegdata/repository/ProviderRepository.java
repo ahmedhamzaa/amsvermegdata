7419ddbeba7d1730675269adedd9c48201f62abd
ahmed hamza jandoubi <ahmedhamza0601@gmail.com>
2021-12-04 10:46:34 +0100

    change git position

--- amsvermegdata/src/main/java/com/sip/amsvermegdata/repository/ProviderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sip.amsvermegdata.repository;
-
-import com.sip.amsvermegdata.entities.Provider;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProviderRepository extends CrudRepository<Provider, Long> {
-}
