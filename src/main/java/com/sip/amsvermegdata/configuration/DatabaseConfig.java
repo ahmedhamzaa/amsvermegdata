427c2d499f9c9d506f11c4c81592db2c1f048b74
ahmed hamza jandoubi <ahmedhamza0601@gmail.com>
2021-12-04 11:13:29 +0100

    data base config

--- src/main/java/com/sip/amsvermegdata/configuration/DatabaseConfig.java
+++ src/main/java/com/sip/amsvermegdata/configuration/DatabaseConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 public class DatabaseConfig {
     @Value("${spring.datasource.url}")
-    private String dbUrl="ec2-34-255-134-200.eu-west-1.compute.amazonaws.com";
+    private String dbUrl="postgres://nmhygqasayofri:057d0982715d3f2653ed4d0b9d16785d9ee261cfab42ca5cbdad0584dee622fe@ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/d17fasebjlqgfn\n";
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
